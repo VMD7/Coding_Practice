c8e25c4ccfa0504a91a07fe35bc5c5a61defaaa5
VMD7 <vijaydevane007@gmail.com>
2021-07-20 22:03:46 +0530

    other

--- Others/prime_sum_bt_num.cpp
+++ Others/prime_sum_bt_num.cpp
@@ -1,27 +1,19 @@
-//Take sum of all prime numbers between two numbers
+//Print n fabanocci numbers
 #include <iostream>
-#include <cmath>
 using namespace std;
-bool isPrime(int n){
-    int i;
-    if(n==1){
-        return 0;
-    }
-    for(i=2;i<=sqrt(n);i++){
-        if(n%i==0){
-            return 0;
-        }
-    }
-    return 1;
-}
+
 
 int main(){
-    int a,b,i,sum=0;
-    cin>>a>>b;
-    for(i=a;i<=b;i++){
-        if(isPrime(i)){
-            sum=sum+i;
+    int n;
+    cin>>n;
+    for(i=0;i<n;i++){
+        if(i==0)
+        cout<<0;
+        if(i==1)
+        cout<<1;
+        else{
+            i= i-1 + i-2;
+            cout<<i;
         }
     }
-    cout<<sum;
 }
