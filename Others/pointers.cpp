5358a681cc5d2e4ff65cba373c14d7d33888bad7
VMD7 <vijaydevane007@gmail.com>
2021-07-28 11:22:22 +0530

    pointers operations

---
+++ Others/pointers.cpp
@@ -0,0 +1,14 @@
+//pointers operations
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=10;
+    int *ptr;
+    ptr=&n;
+    cout<<ptr<<endl;
+    cout<<*ptr<<endl;
+    cout<<n<<endl;
+    
+}
